7738c4cc396bf8964beaf6d49a6221986e5d4a92
daisymilan <daisymilan3@gmail.com>
2024-07-10 22:25:08 +0800

    class components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,22 @@
 import { RecipeList } from './RecipeList/RecipeList';
 import recipes from '../data/recipes.json';
+import { Component } from 'react';
 
-export const App = () => {
-  return <RecipeList recipes={recipes} />;
+export class App extends Component {
+  handleClick = () => {
+    console.log('Hello Im Clicked');
+  };
+handleClickTest = name => {
+  console.log(`Hello! I'm clicked by ${name}`);
 };
+
+render() {
+  return (
+    <>
+      <RecipeList recipes={recipes} />
+      <button onClick={this.handleClick}>Click Me</button>
+      <button onClick={() => this.handleClickTest('daisy')}>Click Me</button>
+    </>
+  );
+}
+}
